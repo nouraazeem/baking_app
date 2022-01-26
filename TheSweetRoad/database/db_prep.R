a5f31c6671eccaacf155dc7ba210ba46a844dabd
Noura <noura.azeem@gmail.com>
2022-01-26 15:31:43 -0500

    COMMIT - WIP, but uploading anyways to not lose any progress

--- TheSweetRoad/database/db_prep.R
+++ TheSweetRoad/database/db_prep.R
@@ -1,26 +1,40 @@
-save_data_mysql <- function(data) {
-  db <- dbConnect(MySQL(), dbname = "bitdotio", host = "db.bit.io", 
-                  port = 5432, user = "nouraazeem_demo_db_connection", 
-                  password = "auwT_GGE2VKYG7LhBkj63XbK6wEU"
-  query <- sprintf("INSERT INTO %s (%s) VALUES ('%s')", TABLE_NAME, 
-                   paste(names(data), collapse = ", "), paste(data, collapse = "', '"))
-  dbGetQuery(db, query)
-  dbDisconnect(db)
-}
+# save_data_mysql <- function(data) {
+#   db <- dbConnect(MySQL(), dbname = "bitdotio", host = "db.bit.io", 
+#                   port = 5432, user = "nouraazeem_demo_db_connection", 
+#                   password = "auwT_GGE2VKYG7LhBkj63XbK6wEU"
+#   query <- sprintf("INSERT INTO %s (%s) VALUES ('%s')", TABLE_NAME, 
+#                    paste(names(data), collapse = ", "), paste(data, collapse = "', '"))
+#   dbGetQuery(db, query)
+#   dbDisconnect(db)
+# }
 ######
 
+Sys.setenv(PGGSSENCMODE="disable")
+
+
+con <- dbConnect(RPostgres::Postgres(), dbname = 'bitdotio', 
+                 host = 'db.bit.io',
+                 port = 5432,
+                 user = 'bitdotio',
+                 password = rstudioapi::askForPassword('Database Password'))
+
+# Construct the update query by looping over the data fields
+query <- 'INSERT INTO "nouraazeem/baking_recipes"."ingredients_needed" 
+  (recipe_name, amount_1, ingredient_1, amount_2, ingredient_2, amount_3, ingredient_3, 
+  amount_4, ingredient_4, amount_5, ingredient_5, amount_6, ingredient_6, amount_7,
+  ingredient_7) VALUES (1,2,3,1,1,1,1,1,1,1,1,1,1,1,1)'
+
+
+#("Palestinian Knafe","1 Package", "Shredded Phyllo Dough", "16 ounces", 
+ #"Fresh Mozzarella Slices", "1/2 - 1 Cup", "Ricotta Cheese", "2 Sticks", "Butter", "1 Cup",
+ #"Sugar", "1", "Lemon") 
+
+query <- 'SELECT * FROM "nouraazeem/baking_recipes"."ingredients_needed"'
+ex <- dbGetQuery(con, query)
 
 
 
 
-options(mysql = list(
-  "host" = "db.bit.io",
-  "port" = 5432,
-  "user" = "nouraazeem_demo_db_connection",
-  "password" = "auwT_GGE2VKYG7LhBkj63XbK6wEU"
-))
-databaseName <- "bitdotio"
-table <- "ingredients_needed"
 
 saveData <- function(data) {
   # Connect to the database
